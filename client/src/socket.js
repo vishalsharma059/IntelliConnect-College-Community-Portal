d514b763f81abb91bd876360d6e4c94f3d078ab7
Vishal Sharma <vishalsharma02731@gmail.com>
2025-05-18 04:29:15 +0530

    Update socket.js

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 const SOCKET_URL = process.env.REACT_APP_SOCKET_URL;
 
-const socket = io(SOCKET_UR); // matches your server
+const socket = io(SOCKET_URL); // matches your server
 
 export default socket;
