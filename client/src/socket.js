6f284b6e5bb794b6d45dbbf1de3481e31859543a
Vishal Sharma <vishalsharma02731@gmail.com>
2025-05-18 03:40:37 +0530

    Update socket.js

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,5 +1,6 @@
 import { io } from "socket.io-client";
+const SOCKET_URL = process.env.REACT_APP_SOCKET_URL;
 
-const socket = io("https://intelliconnect-college-community-portal.onrender.com"); // matches your server
+const socket = io(SOCKET_UR); // matches your server
 
 export default socket;
